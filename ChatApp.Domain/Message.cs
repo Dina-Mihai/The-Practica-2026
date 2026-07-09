9417b6b9d048d564dff871c616f8d131e7137f69
Dina-Mihai <dinamihai1602@gmail.com>
2026-07-09 19:00:40 +0300

    define entites

---
+++ ChatApp.Domain/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Domain
+{
+    internal class Message
+    {
+        
+    }
+   
+}
+/* continut
+ * id
+ * ora timiteri
+ * user
+ * catre cine
+ */ 
---
+++ ChatApp.Domain/User.cs
@@ -0,0 +1,17 @@
+﻿namespace ChatApp.Domain
+{
+    public class User
+    {
+
+    }
+}
+/*clase: user:-nume
+ *            -parola
+ *            -adresa de mail
+ *            -user name
+ *            -#poza
+ *            -id
+ *            -#data de creare
+ *       
+ *  
+ */
