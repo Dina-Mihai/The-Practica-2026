45b587c486d17ac8d52b661556bf02dc4cf40fb9
Dina-Mihai <dinamihai1602@gmail.com>
2026-07-15 11:52:25 +0300

    added a message and a user class

---
+++ ChatApp.Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChatApp.Domain.Entities
+{
+    public class User
+    {
+        String Name;
+        String Password;//care este diferenta
+        string Email;
+        float  Id;
+    }
+}
+
+
+/*clase: user:-nume
+ *            -parola
+ *            -adresa de mail
+ *            -user name
+ *            -#poza
+ *            -id
+ *            -#data de creare
+ */
+//nu am acces la fiserul README.mb in visualstudio
