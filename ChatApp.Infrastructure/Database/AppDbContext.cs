287e6b5682161368f744e87ba884f6d4df30fe3f
Dina-Mihai <dinamihai1602@gmail.com>
2026-07-22 17:08:19 +0300

    I added Database

---
+++ ChatApp.Infrastructure/Database/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using ChatApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Infrastructure.Database
+{
+    class AppDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        public string DbPath { get;}
+
+        public AppDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "blogging.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={DbPath}");
+
+    }
+}
